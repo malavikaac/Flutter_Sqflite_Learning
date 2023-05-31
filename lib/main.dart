c7115f06e2efa19870a747c6830ef2101c03254c
malavikaac <acmalavika@gmail.com>
2023-05-31 11:25:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+import 'add_film.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Films',
+      home: Home() ,
+      routes: {
+        "addfilm":(context)=>AddFilm(),
+      },
+    );
+  }
+}
